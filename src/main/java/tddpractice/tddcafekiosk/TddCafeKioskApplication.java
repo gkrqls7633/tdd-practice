a88e3e61aca61cf045847e9ccbf57d8be30ee1de
hakbinMac <gkrqls8409@gmail.com>
2025-02-20 22:55:53 +0900

    initial commit

---
+++ src/main/java/tddpractice/tddcafekiosk/TddCafeKioskApplication.java
@@ -0,0 +1,13 @@
+package tddpractice.tddcafekiosk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddCafeKioskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TddCafeKioskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tddpractice/tddcafekiosk/TddCafeKioskApplicationTests.java
@@ -0,0 +1,13 @@
+package tddpractice.tddcafekiosk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddCafeKioskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
