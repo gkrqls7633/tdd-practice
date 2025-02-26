75e8b5cc00e02bb914e22eda860c4e903f767865
hakbinMac <gkrqls8409@gmail.com>
2025-02-26 19:05:12 +0900

    향후 spring & jpa 기반 테스트 작성 위한 spring 폴더 분리

--- src/main/java/tddpractice/tddcafekiosk/spring/TddCafeKioskApplication.java
+++ src/main/java/tddpractice/tddcafekiosk/spring/TddCafeKioskApplication.java
@@ -1,4 +1,4 @@
-package tddpractice.tddcafekiosk;
+package tddpractice.tddcafekiosk.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
