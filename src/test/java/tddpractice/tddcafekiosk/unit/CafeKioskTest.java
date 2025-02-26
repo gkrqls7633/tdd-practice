556ef031f4a9f4f10dec4312ab708a7c69c197f7
hakbinMac <gkrqls8409@gmail.com>
2025-02-26 18:44:56 +0900

    [스터디] 각자 CafeKiosk, 음료 도메인 테스트 코드 작성해보는 용도

--- src/test/java/tddpractice/tddcafekiosk/unit/CafeKioskTest.java
+++ src/test/java/tddpractice/tddcafekiosk/unit/CafeKioskTest.java
@@ -5,11 +5,19 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+
+/*
+ * CafeKiosk 클래스의 각각의 메소드를 테스트 코드로 작성해야한다.
+ * 실패 케이스를 먼저 고려하여 테스트 코드를 작성하는 습관을 들여보자.
+ */
+
 class CafeKioskTest {
 
     @DisplayName("키오스크 주문 목록에 상품을 추가한다.")
     @Test
     void add() {
     }
+
+
 
 }
---
+++ src/test/java/tddpractice/tddcafekiosk/unit/beverage/LatteTest.java
@@ -0,0 +1,11 @@
+package tddpractice.tddcafekiosk.unit.beverage;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/*
+ * Latte class 를 테스트코드로 작성할 수 있다.
+ * 간단한 entity class 이지만 충분히 테스트 할 가치가 있다.
+ */
+class LatteTest {
+
+}
