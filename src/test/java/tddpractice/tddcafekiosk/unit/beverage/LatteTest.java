b526cfe18195ac95db3390a0441dcde59dabb7e5
hakbinMac <gkrqls8409@gmail.com>
2025-03-01 18:13:55 +0900

    엔티티 테스트 추가, order 생성 조건 추가 (음료 목록 최소 1 이상)

--- src/test/java/tddpractice/tddcafekiosk/unit/beverage/LatteTest.java
+++ src/test/java/tddpractice/tddcafekiosk/unit/beverage/LatteTest.java
@@ -1,5 +1,9 @@
 package tddpractice.tddcafekiosk.unit.beverage;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 /*
@@ -8,4 +12,24 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 class LatteTest {
 
+
+    @DisplayName("라떼 이름은 라떼다.")
+    @Test
+    void latteNameCheck() {
+        //given
+        Latte latte = new Latte();
+
+        //when & then
+        assertThat(latte.getName()).isEqualTo("라떼");
+    }
+
+    @DisplayName("라떼 가격은 4500원이다.")
+    @Test
+    void lattePriceCheck() {
+        //given
+        Latte latte = new Latte();
+
+        //when & then
+        assertThat(latte.getPrice()).isEqualTo(4500);
+    }
 }
