13ae8e503ad13d3e8f47f75d57f1be8cbc93f32e
hakbinMac <gkrqls8409@gmail.com>
2025-03-08 18:33:58 +0900

    재고 차감 실패 케이스 테스트코드 추가; 재고가 부족하면 주문 생성은 불가

--- src/test/java/tddpractice/tddcafekiosk/spring/api/service/order/OrderServiceTest.java
+++ src/test/java/tddpractice/tddcafekiosk/spring/api/service/order/OrderServiceTest.java
@@ -11,11 +11,13 @@ import tddpractice.tddcafekiosk.spring.domain.product.Product;
 import tddpractice.tddcafekiosk.spring.domain.product.ProductRepository;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static tddpractice.tddcafekiosk.spring.domain.product.ProductSellingStatus.HOLD;
 import static tddpractice.tddcafekiosk.spring.domain.product.ProductSellingStatus.SELLING;
@@ -62,7 +64,7 @@ public class OrderServiceTest {
         Map<String, Integer> productNumberCounts = productList.stream()
                 .collect(Collectors.toMap(
                         Product::getProductNumber,
-                        p -> 1, // 기본 개수를 1로 설정
+                        p -> 1, // 1개씩 주문
                         Integer::sum // 같은 productNumber가 있으면 개수를 합산
                 ));
 
@@ -79,40 +81,43 @@ public class OrderServiceTest {
         assertThat(orderResponse.getProducts().size()).isEqualTo(2);
     }
 
-//    @DisplayName("상품 번호 리스트가 빈 값이면 주문을 생성할 수 없다.")
-//    @Test
-//    void createOrderWithNoProducts() {
-//
-//        // given
-//        Product product1 = Product.builder()
-//                .productNumber("001")
-//                .type(HANDMADE)
-//                .sellingStatus(SELLING)  //판매상태
-//                .name("아메리카노")
-//                .price(4000)
-//                .build();
-//
-//        Product product2 = Product.builder()
-//                .productNumber("002")
-//                .type(HANDMADE)
-//                .sellingStatus(HOLD)  //판매상태
-//                .name("카페라떼")
-//                .price(4500)
-//                .build();
-//
-//        List<Product> productList = List.of(product1, product2);
-//        productRepository.saveAll(productList);
-//
-//        OrderCreateRequest orderCreateRequest = OrderCreateRequest.builder()
-//                .productNumbers(productList.stream()
-//                        .map(Product::getProductNumber)
-//                        .collect(Collectors.toList()))
-//                .build();
-//
-//        LocalDateTime registeredDateTime = LocalDateTime.now();
-//
-//        //when
-//        OrderResponse orderResponse = orderService.createOrder(orderCreateRequest, registeredDateTime);
-//
-//    }
+    @DisplayName("재고가 부족하면 주문 생성은 불가하다.")
+    @Test
+    void cannotCreateOrderWithInsufficientStock() {
+        // given
+        Product product1 = Product.builder()
+                .productNumber("001")
+                .type(HANDMADE)
+                .sellingStatus(SELLING)
+                .name("아메리카노")
+                .price(4000)
+                .stockCount(1) //현재 재고
+                .build();
+
+        Product product2 = Product.builder()
+                .productNumber("002")
+                .type(HANDMADE)
+                .sellingStatus(HOLD)
+                .name("카페라떼")
+                .price(4500)
+                .stockCount(1) // 현재 재고
+                .build();
+
+        List<Product> productList = List.of(product1, product2);
+        productRepository.saveAll(productList);
+
+        OrderCreateRequest orderCreateRequest = new OrderCreateRequest();
+        Map<String, Integer> orderCreateRequestMap = new HashMap<>();
+        orderCreateRequestMap.put("001", 1);
+        orderCreateRequestMap.put("002", 2);
+        orderCreateRequest.setProductNumberCounts(orderCreateRequestMap);
+
+        LocalDateTime registeredDateTime = LocalDateTime.now();
+
+        // when & then
+        //라떼의 재고는 1인데 2개 주문했으므로 에러 발생해야함.
+        assertThatThrownBy(() -> orderService.createOrder(orderCreateRequest, registeredDateTime))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("재고 수량보다 주문 수가 많습니다.");
+    }
 }
